03f38f64980f7b2527b9959c9e8bdf7fac63e3a8
metaxiii <lehchibi.gael@gmail.com>
2018-03-03 18:29:28 +0100

    Modification test

--- www/index.php
+++ www/index.php
@@ -5,4 +5,17 @@
  * Date: 03/03/2018
  * Time: 16:37
  */
+?>
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Food 'n Drink </title>
+</head>
+<body>
 
+</body>
+</html>
