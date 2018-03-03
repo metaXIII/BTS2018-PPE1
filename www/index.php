ab3a97f2c1b87a0ecd700b5ba55123ff2997c1f1
metaxiii <lehchibi.gael@gmail.com>
2018-03-03 19:39:37 +0100

    Add footer to "includes"

--- www/index.php
+++ www/index.php
@@ -15,7 +15,7 @@ require 'fonctions/fonction.php';
 </head>
 
 <body>
-
+<?php require 'includes/header.php'; ?>
 <main role="main" class="container">
 
     <div class="row row-offcanvas row-offcanvas-right">
@@ -95,20 +95,7 @@ require 'fonctions/fonction.php';
 
 </main><!--/.container-->
 
-<footer>
-    <p>&copy; Company 2017</p>
-</footer>
+<?php require 'includes/footer.php' ?>
 
-
-<!-- Bootstrap core JavaScript
-================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-        crossorigin="anonymous"></script>
-<script>window.jQuery || document.write('<script src="js/jquery-3.3.1.min.js"><\/script>')</script>
-<script src="js/popper.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/offcanvas.js"></script>
 </body>
 </html>
