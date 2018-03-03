f1bf599e58d0c1bb380958f84bbc118d7ce14f45
metaxiii <lehchibi.gael@gmail.com>
2018-03-03 19:33:56 +0100

    Add Head file

--- www/includes/head.php
+++ www/includes/head.php
@@ -3,5 +3,17 @@
  * Created by PhpStorm.
  * User: Metaxiii
  * Date: 03/03/2018
- * Time: 19:20
+ * Time: 19:32
  */
+?>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="Site de cuisine - découverte culinaire">
+<meta name="author" content="metaXIII">
+<link rel="icon" href="images/Wallpapers/icone.jpg">
+<title>Off Canvas Template for Bootstrap</title>
+<!-- Bootstrap core CSS -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<!-- Custom styles for this template -->
+<link href="css/offcanvas.css" rel="stylesheet">
+<link rel="stylesheet" href="css/style.css">
--- www/index.php
+++ www/index.php
@@ -13,17 +13,7 @@ if ($db)
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="Site de cuisine - découverte culinaire">
-    <meta name="author" content="metaXIII">
-    <link rel="icon" href="images/Wallpapers/icone.jpg">
-    <title>Off Canvas Template for Bootstrap</title>
-    <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <!-- Custom styles for this template -->
-    <link href="css/offcanvas.css" rel="stylesheet">
-    <link rel="stylesheet" href="css/style.css">
+    <?php require 'includes/head.php'; ?>
 </head>
 
 <body>
