2cc767a70a77843fc332c23ac2e14754896f1bca
metaxiii <lehchibi.gael@gmail.com>
2018-03-03 19:41:37 +0100

    Correct footer and index

--- www/includes/footer.php
+++ www/includes/footer.php
@@ -9,15 +9,3 @@
 <footer>
     <p>&copy; Company 2017</p>
 </footer>
-
-
-<!-- Bootstrap core JavaScript
-================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-        crossorigin="anonymous"></script>
-<script>window.jQuery || document.write('<script src="js/jquery-3.3.1.min.js"><\/script>')</script>
-<script src="js/popper.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/offcanvas.js"></script>
